dde5b8a7963f45dce8be5b1076afdb35e50fc377
InhyeKang <kanginhae0826@gmail.com>
2023-09-15 14:47:29 +0900

    수업 예제 풀이

--- class.py
+++ class.py
@@ -56,22 +56,105 @@
 # print(l)
 
 
-icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
-ice = icecream.keys()
-print(ice)
-print(type(ice))
+# icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+# ice = icecream.keys()
+# print(ice)
+# print(type(ice))
 
-name1 = "김민수" 
-age1 = 10
-name2 = "이철희"
-age2 = 13
+# name1 = "김민수" 
+# age1 = 10
+# name2 = "이철희"
+# age2 = 13
 
-print("이름: {} 나이: {}".format(name1, age1))
-print("이름: {} 나이: {}".format(name2, age2))
+# print("이름: {} 나이: {}".format(name1, age1))
+# print("이름: {} 나이: {}".format(name2, age2))
 
-print(f"이름: {name1} 나이: {age1}")
-print(f"이름: {name2} 나이: {age2}")
+# print(f"이름: {name1} 나이: {age1}")
+# print(f"이름: {name2} 나이: {age2}")
 
 
-상장주식수 = "5,969,782,550"
-print(상장주식수.replace(",", ""))
+# 상장주식수 = "5,969,782,550"
+# print(상장주식수.replace(",", ""))
+
+
+
+
+money = 3000
+
+if money >= 3000:
+    print("taxi")
+else:
+    print("walk")
+
+
+pocket = ['paper', 'cellphone', 'money', 'card']
+
+if 'money' not in pocket:
+    print("walk")
+else:
+    print("walk")
+
+
+# 백준 오븐 시계(2525)
+a, b = map(int, input().split())
+c = int(input())
+print(((a*60+b+c)//60)%24, (a*60+b+c)%60)
+
+
+# 백준 알람 시계(2884)
+h, m = map(int, input().split())
+sum = h*60+m-45
+print(sum//60%24, sum%60)
+
+
+score = [90, 25, 67, 45, 80]
+for i in score: 
+    if i >= 80: 
+        print("%d번 학생은 합격입니다." % (score.index(i) + 1))
+    else: 
+        print("%d번 학생은 불합격입니다." % (score.index(i) + 1))
+
+
+score = [90, 25, 67, 45, 80]
+for i in score: 
+    if i <= 50: 
+        print("%d번 학생 보충학습입니다" % (score.index(i) + 1))
+    else: 
+        continue
+
+
+temp = [i for i in range(11) if (i%2)!=0 ]
+print(temp)
+print(sum(temp))
+
+
+coke = 10
+
+while 1:
+    pay = int(input())
+
+    if coke <= 0:
+        print("콜라가 다 떨어졌습니다.\n")
+        print("%d원을 돌려드립니다." %pay)
+    elif coke > 0 & pay == 300:
+        print("콜라를 드립니다.")
+    elif coke > 0 & pay > 300:
+        print("콜라를 드립니다.\n")
+        print("잔돈은 %d원입니다" %pay-300)
+    elif pay < 300:
+        print("금액이 부족합니다.\n")
+        print("%d원을 돌려드립니다." %pay)
+
+
+member = int(input())
+people = []
+
+for _ in range(member):
+    age, name = input().split()
+    age = int(age)
+    people.append((age, name))
+
+people = sorted(people, key=lambda x: x[0])
+
+for p in people:
+    print(p[0], p[1])
